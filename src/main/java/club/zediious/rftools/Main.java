a0bc448fc05b135df2016d3a089705d090d2fa87
zediious <zediious@aol.com>
2022-07-27 21:57:23 -0400

    Concatenation > text block
    Changed back to jdk17, so can use this

--- src/main/java/club/zediious/rftools/Main.java
+++ src/main/java/club/zediious/rftools/Main.java
@@ -16,10 +16,11 @@ public class Main {
     static void idle() throws IOException {
 
         Scanner userInput = new Scanner(System.in);
-        System.out.println("\nEnter a number corresponding to the tool you would like to use;\n" +
-                "1) FindCovers - Find all nodes of a given type that have covers.\n" +
-                "2) PullCovers - Remove all covers from a given node type.\n" +
-                "3) Exit");
+        System.out.println("""
+                Enter a number corresponding to the tool you would like to use;
+                1) FindCovers - Find all nodes of a given type that have covers.
+                2) PullCovers - Remove all covers from a given node type.
+                3) Exit""");
         String userChoice = userInput.nextLine();
 
         switch (userChoice) {
